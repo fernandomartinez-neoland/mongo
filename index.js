1c2f29ad6a8b5824a08475b04bf17c5f1d6cbcee
fernandomartinez-neoland <fernando.martinez@neoland.es>
2026-02-09 17:05:23 +0100

    conexion

--- index.js
+++ index.js
@@ -1,1 +1,4 @@
-console.log("Hola Mundo!")
+import db from 'mongoose';
+
+db.connect('mongodb+srv://usuario_prueba:123@cluster0.w6ghmta.mongodb.net/marketplace')
+.then(() => console.log('Connected!'));
