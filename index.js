0eee181c3b3f17b996b8fef860a2e5f37b50908a
fernandomartinez-neoland <fernando.martinez@neoland.es>
2026-02-10 11:53:34 +0100

    crear coleccion

--- index.js
+++ index.js
@@ -1,4 +1,19 @@
 import db from 'mongoose';
 
 db.connect('mongodb+srv://usuario_prueba:123@cluster0.w6ghmta.mongodb.net/marketplace')
-.then(() => console.log('Connected!'));
+.then(() => console.log('Connected!'))
+.catch((e)=>console.log(e.message));
+
+const Schema= db.Schema;
+
+const UserSchema=new Schema({
+    "nombre":String,
+    "correo": {
+        type:"String",
+        require:true,
+        unique:true
+    },
+    "direccion":String
+});
+
+const UserModel=db.model('UserModel', UserSchema, 'UserModel')
